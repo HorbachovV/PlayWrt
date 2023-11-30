ddedea1b8863d137ca90ad8fde4732da1814a048
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2023-11-30 20:50:48 +0200

    add webtable test

---
+++ tests/webtable.spec.js
@@ -0,0 +1,71 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { table } = require('console');
+const exp = require('constants');
+
+// npx playwright test webtable.spec.js --project=chromium --headed --debug
+
+
+test('has title', async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/');
+
+    const table = await page.locator("#productTable");
+    const columns = await table.locator("thead tr th");
+    console.log("Number of columns:", await columns.count());
+
+    const rows = await table.locator("tbody tr");
+    console.log("Number of rows:", await rows.count());
+
+    expect(await columns.count()).toBe(4);
+    expect(await rows.count()).toBe(5);
+
+    const machedRow = rows.filter({
+        has: page.locator("td"),
+        hasText: "Product 4"
+    })
+    await machedRow.locator("input").check();
+
+    await selectProduct(rows, page, "Product 1");
+    await selectProduct(rows, page, "Product 3");
+    await selectProduct(rows, page, "Product 5");
+
+    for (let i = 0; i < await rows.count(); i++) {
+
+        const row = rows.nth(i);
+        const tds = row.locator("td");
+
+        for (let j = 0; j < await tds.count() - 1; j++) {
+            console.log(await tds.nth(j).textContent());
+        }
+    }
+
+    const pages = await page.locator('.pagination li a');
+    console.log('Number of pages:', await pages.count());
+
+    for (let p = 0; p < await pages.count(); p++) {
+        if(p > 0) {
+            await pages.nth(p).click();
+        }
+        for (let i = 0; i < await rows.count(); i++) {
+
+            const row = rows.nth(i);
+            const tds = row.locator("td");
+    
+            for (let j = 0; j < await tds.count() - 1; j++) {
+                console.log(await tds.nth(j).textContent());
+            }
+        }
+    }
+
+    await page.waitForTimeout(3000)
+});
+
+
+
+async function selectProduct (rows, page, name) {
+    const machedRow = rows.filter({
+        has: page.locator("td"),
+        hasText: name
+    })
+    await machedRow.locator("input").check();
+}
